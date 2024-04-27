e7087c1104cbf8c1a8488c66b56b4ea7ce77c21a
Victortaeijmans <victortaeijmans@gmail.com>
2024-04-27 22:37:35 +0200

    pimpl fix SDL2_mixer

--- Minigin/SoundSystemSDL.cpp
+++ Minigin/SoundSystemSDL.cpp
@@ -1,91 +1,148 @@
 #include "SoundSystemSDL.h"
 
-#include <assert.h>
+#include <cassert>
 #include <iostream>
 #include <SDL.h>
-#include <stdexcept>
-
 #include <SDL_mixer.h>
+#include <stdexcept>
+#include "ThreadQueueWorker.h"
 
+#include <unordered_map>
 
-minigin::SoundSystemSDL::SoundSystemSDL()
+namespace minigin
 {
-    //Initialize all SDL subsystems
-    if (SDL_Init(SDL_INIT_AUDIO) != 0)
-    {
-        throw std::runtime_error(std::string("SDL_mixer_Init Error: ") + SDL_GetError());
-    }
-    //Initialize SDL_mixer
-    if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) != 0)
-    {
-        throw std::runtime_error(std::string("SDL_mixer_Init Error: ") + Mix_GetError());
-    }
-
-
-
-    std::function soundProcessFunc = [this](const SoundCommand& task) -> bool
-		{ return this->ProcessSoundRequest(task); };
-
-    m_ThreadedTaskProcessor
-		= std::make_unique<ThreadQueueWorker<SoundCommand>>(soundProcessFunc);
-}
-
-minigin::SoundSystemSDL::~SoundSystemSDL()
-{
-	for (Mix_Chunk* loadedSound : m_LoadedSounds)
+	// PIPMP IMPL
+	class SoundSystemSDL::SDLImpl final : public ISoundSystem
 	{
-		Mix_FreeChunk(loadedSound);
-	}
-	Mix_Quit();
-}
+	public:
+		SDLImpl();
+		~SDLImpl() override;
+
+		SDLImpl(const SDLImpl&) = delete;
+		SDLImpl(SDLImpl&&) = delete;
+		SDLImpl& operator=(const SDLImpl&) = delete;
+		SDLImpl& operator=(SDLImpl&&) = delete;
+
+		unsigned int LoadSound(const std::string& path) override;
+		void PlaySound(unsigned soundID, float volume) override;
+
+	private:
+		struct SoundCommand
+		{
+			unsigned int id;
+			float volume;
+		};
+
+		bool ProcessSoundRequest(const SoundCommand& command) const;
+
+		bool SDLLoadSound(const std::string& path);
 
 
-unsigned int minigin::SoundSystemSDL::LoadSound(const std::string& path)
-{
-	if(const auto id = m_SoundTable.find(path); id != m_SoundTable.end())
-    {
-        return id->second;
-    }
 
+		std::unique_ptr<ThreadQueueWorker<SoundCommand>> m_ThreadedTaskProcessor;
 
-	if(SDLLoadSound(path))
+		std::unordered_map<std::string, unsigned int> m_SoundTable;
+		std::vector<Mix_Chunk*> m_LoadedSounds;
+	};
+
+	SoundSystemSDL::SDLImpl::SDLImpl()
 	{
-		const unsigned int soundID{ static_cast<unsigned>(m_LoadedSounds.size() - 1) };
-        m_SoundTable[path] = soundID;
-        return soundID;
+		//Initialize all SDL subsystems
+		if (SDL_Init(SDL_INIT_AUDIO) != 0)
+		{
+			throw std::runtime_error(std::string("SDL_mixer_Init Error: ") + SDL_GetError());
+		}
+		//Initialize SDL_mixer
+		if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) != 0)
+		{
+			throw std::runtime_error(std::string("SDL_mixer_Init Error: ") + Mix_GetError());
+		}
+
+
+
+		std::function soundProcessFunc = [this](const SoundCommand& task) -> bool
+			{ return this->ProcessSoundRequest(task); };
+
+		m_ThreadedTaskProcessor
+			= std::make_unique<ThreadQueueWorker<SoundCommand>>(soundProcessFunc);
+	}
+
+	SoundSystemSDL::SDLImpl::~SDLImpl()
+	{
+		for (Mix_Chunk* loadedSound : m_LoadedSounds)
+		{
+			Mix_FreeChunk(loadedSound);
+		}
+		Mix_Quit();
 	}
 
 
-	return 0;
+	unsigned int SoundSystemSDL::SDLImpl::LoadSound(const std::string& path)
+	{
+		if (const auto id = m_SoundTable.find(path); id != m_SoundTable.end())
+		{
+			return id->second;
+		}
+
+
+		if (SDLLoadSound(path))
+		{
+			const unsigned int soundID{ static_cast<unsigned>(m_LoadedSounds.size() - 1) };
+			m_SoundTable[path] = soundID;
+			return soundID;
+		}
+
+
+		return 0;
+	}
+
+	void SoundSystemSDL::SDLImpl::PlaySound(unsigned soundID, float volumePercentage = 1)
+	{
+		m_ThreadedTaskProcessor->AddTask(SoundCommand{ soundID, volumePercentage });
+	}
+
+
+	bool SoundSystemSDL::SDLImpl::ProcessSoundRequest(const SoundCommand& command) const
+	{
+		const int usedChannel = Mix_PlayChannel(-1, m_LoadedSounds[command.id], 0);
+		Mix_Volume(usedChannel, int(command.volume * MIX_MAX_VOLUME));
+
+		return (usedChannel != -1);
+	}
+
+
+	bool SoundSystemSDL::SDLImpl::SDLLoadSound(const std::string& path)
+	{
+		Mix_Chunk* loadedSound = Mix_LoadWAV(path.data());
+
+		if (loadedSound == NULL)
+		{
+			assert(false);
+			std::cout << "failed to load sound: " + path + "\n";
+			return false;
+		}
+
+		m_LoadedSounds.push_back(loadedSound);
+		return true;
+	}
+
+	//SoundSystemSDL
+
+	SoundSystemSDL::SoundSystemSDL()
+		: m_Impl{std::make_unique<SDLImpl>()}
+	{
+	}
+
+
+	SoundSystemSDL::~SoundSystemSDL() = default;
+
+	unsigned SoundSystemSDL::LoadSound(const std::string& path)
+	{
+		return m_Impl->LoadSound(path);
+	}
+
+	void SoundSystemSDL::PlaySound(unsigned soundID, float volume)
+	{
+		m_Impl->PlaySound(soundID, volume);
+	}
 }
-
-void minigin::SoundSystemSDL::PlaySound(unsigned soundID, float volumePercentage = 1)
-{
-    m_ThreadedTaskProcessor->AddTask(SoundCommand{ soundID, volumePercentage });
-}
-
-
-bool minigin::SoundSystemSDL::ProcessSoundRequest(const SoundCommand& command) const
-{
-    const int usedChannel = Mix_PlayChannel(-1, m_LoadedSounds[command.id], 0);
-    Mix_Volume(usedChannel, int(command.volume * MIX_MAX_VOLUME));
-    
-    return (usedChannel != -1);
-}
-
-
-bool minigin::SoundSystemSDL::SDLLoadSound(const std::string& path)
-{
-    Mix_Chunk* loadedSound = Mix_LoadWAV(path.data());
-
-    if(loadedSound == NULL)
-    {
-	    assert(false);
-        std::cout << "failed to load sound: " + path + "\n";
-        return false;
-    }
-
-    m_LoadedSounds.push_back(loadedSound);
-    return true;
-}
-
--- Minigin/SoundSystemSDL.h
+++ Minigin/SoundSystemSDL.h
@@ -1,45 +1,27 @@
 #pragma once
 #include <string>
-#include <unordered_map>
-
+#include <memory>
 #include "ISoundSystem.h"
-#include "ThreadQueueWorker.h"
 
-struct Mix_Chunk;
 namespace minigin
 {
 
-class SoundSystemSDL final : public ISoundSystem
-{
-public:
-	SoundSystemSDL();
-	~SoundSystemSDL() override;
-
-	SoundSystemSDL(const SoundSystemSDL&) = delete;
-	SoundSystemSDL(SoundSystemSDL&&) = delete;
-	SoundSystemSDL& operator=(const SoundSystemSDL&) = delete;
-	SoundSystemSDL& operator=(SoundSystemSDL&&) = delete;
-
-	unsigned int LoadSound(const std::string& path) override;
-	void PlaySound(unsigned soundID, float volume) override;
-
-private:
-	struct SoundCommand
+	class SoundSystemSDL final : public ISoundSystem
 	{
-		unsigned int id;
-		float volume;
+	public:
+		SoundSystemSDL();
+		~SoundSystemSDL() override;
+
+		SoundSystemSDL(const SoundSystemSDL&) = delete;
+		SoundSystemSDL(SoundSystemSDL&&) = delete;
+		SoundSystemSDL& operator=(const SoundSystemSDL&) = delete;
+		SoundSystemSDL& operator=(SoundSystemSDL&&) = delete;
+
+		unsigned int LoadSound(const std::string& path) override;
+		void PlaySound(unsigned soundID, float volume) override;
+
+	private:
+		class SDLImpl;
+		std::unique_ptr<SDLImpl> m_Impl;
 	};
-
-	bool ProcessSoundRequest(const SoundCommand& command) const;
-
-	bool SDLLoadSound(const std::string& path);
-
-
-
-	std::unique_ptr<ThreadQueueWorker<SoundCommand>> m_ThreadedTaskProcessor;
-
-	std::unordered_map<std::string, unsigned int> m_SoundTable;
-	std::vector<Mix_Chunk*> m_LoadedSounds;
-};
-
 }
